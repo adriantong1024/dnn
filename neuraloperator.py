81fad819281739c38b19b4844e0486afc93d5ed0
Adrian Tong <adriantong1024@gmail.com>
2020-02-10 11:36:38 -0800

    Add sigmoid backward function

--- neuraloperator.py
+++ neuraloperator.py
@@ -5,9 +5,12 @@ class Sigmoid:
     def __init__(self):
         return
 
-    def feedforward(self, data):
+    def compute(self, data):
         # 1/(1+e^-z)
         return np.power(1 + np.exp(data), -1)
 
+    def feedforward(self, data):
+        return self.compute(data)
+
     def backward(self, data):
-        return data
+        return np.multiply(self.compute(data), (1 - self.compute(data)))
